c42231b661a2765befeaf2f48897ad7cc7c1375d
bauti <jnemi@fie.undef.edu.ar>
2021-10-16 16:00:38 -0300

    ADD - Assignment 1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print("Hello, world!")
+
+
+def filter_numbers(text):
+    aux = []
+    for word in text.split(" "):
+        try:
+            aux.append(float(word))
+        except ValueError:
+            continue
+    return aux
+
+
+print(filter_numbers("Sin despreciar 13.23 mal vio honestidad envidiaron suspirando435.5. Me alfombra arrojo bronce "
+                     "lo seguia. El cafe es cuna 3 onda. Si 3.2sacar no ya busca dogma sento. "
+                     "Cuarto volado graves iba han. 54.34 tanto"))
